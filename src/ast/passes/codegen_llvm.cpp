0b187d64efd3539df5c19332a70e8965e8e4fc41
Wu Xiangnan <wuxn9999@foxmail.com>
2025-11-06 02:34:22 +0800

    Reduce maximum entries of BPF_MAP_TYPE_LRU_HASH

--- src/ast/passes/codegen_llvm.cpp
+++ src/ast/passes/codegen_llvm.cpp
@@ -3830,7 +3830,7 @@ void CodegenLLVM::generate_maps(const RequiredResources &required_resources,
   for (const StackType &stack_type : codegen_resources.stackid_maps) {
     createMapDefinition(stack_type.name(),
                         libbpf::BPF_MAP_TYPE_LRU_HASH,
-                        128 << 10,
+                        1 << 10,
                         CreateArray(16, CreateInt8()),
                         CreateArray(stack_type.limit, CreateUInt64()));
     max_stack_limit = std::max(stack_type.limit, max_stack_limit);
